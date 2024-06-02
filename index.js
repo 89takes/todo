7e18a818ada41a7432b183b9679a6315414d8a45
“divij.dua@unicommerce.com” <divij.dua@unicommerce.com>
2024-06-02 13:26:51 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { createTodo } = require('./types');
 
 const app = express()
 
@@ -12,11 +13,30 @@ app.get('/todos', function(req, res) {
 // }
 
 app.post('/todo', function(req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "Wrong inputs"
+        })
+        return;
+    }
+
+    // put in mongo
 
 })
 
 app.put('/completed', function(req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updatePayload.safeParse(updatePayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "Wrong inputs"
+        })
+        return;
+    }
 
+    // update in mongo 
 }) 
 
 const PORT = process.env.PORT || 3000
